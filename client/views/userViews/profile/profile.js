d364505d7150fbbec057016b4c4a859fa04ebdef
Victor <lopezv@mit.edu>
2016-01-14 20:29:20 -0500

    groups now show up on profile page

--- client/views/userViews/profile/profile.js
+++ client/views/userViews/profile/profile.js
@@ -4,19 +4,20 @@ Template.profile.helpers({
         return Actions.find({});
     },
     'getGroups' : function () {
-        return Groups.find({_id: {$in: this.groups}});
+        console.log(Groups.find({_id: {$in: Meteor.user().profile.groups}}).fetch());
+        return Groups.find({_id: {$in: Meteor.user().profile.groups}});
     },
     'competitions' : function () {
         // Get a cursor pointing to all the actions and fetch them all
-        var groups = Groups.find({_id: {$in: this.groups}}).fetch();
+        var groups = Groups.find({_id: {$in: Meteor.user().profile.groups}}).fetch();
         return groups.map(function(item){return item.competitions}).filter(function(item){return item.userLevel});
     },
     'has': function(col){
       return col.length > 0;
     },
     'action': function(){
-        var action_data = Actions.find({_id: {$in:this.history.map(function(action){return action.id})}});
-        return this.history.map(function(action){});
+        var action_data = Actions.find({_id: {$in:Meteor.user().history.map(function(action){return action.id})}});
+        return Meteor.user().history.map(function(action){});
     }
 });
 
