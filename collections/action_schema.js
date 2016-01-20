7aa3e005d87ce4d8292bb5a804d17a1a78cb383a
Lawrence Wu <llwu@llwu.me>
2016-01-20 00:38:55 -0500

    action daily cap

--- collections/action_schema.js
+++ collections/action_schema.js
@@ -1,3 +1,7 @@
+/**
+ * @overview Defines and attaches the action schema.
+ */
+
 Actions = new Mongo.Collection('actions');
 
 MULTIPLY = 0;
@@ -5,29 +9,40 @@ ADD = 1;
 
 ActionSchema = new SimpleSchema({
     title: {
-        type: String
+        type: String,
+        label: "High level description of action"
     },
     defaultPoints: {
         type: Number,
-        defaultValue: 0
+        defaultValue: 0,
+        label: "Number of points awarded when all fields are 0"
+    },
+    dailyCap: {
+        type: Number,
+        label: "Number of points a user can gain from this action each day"
     },
     isGlobal: {
-        type: Boolean
+        type: Boolean,
+        label: "Whether the action counts for global points"
     },
     fields: {
         type: [Object],
-        defaultValue: []
+        defaultValue: [],
+        label: "Array of fields a user can fill in"
     },
     "fields.$.name": {
-        type: String
+        type: String,
+        label: "Description of field"
     },
     "fields.$.operation": {
         type: Number,
-        defaultValue: MULTIPLY
+        defaultValue: MULTIPLY,
+        label: "What the field does"
     },
     "fields.$.scale": {
         type: Number,
-        defaultValue: 1
+        defaultValue: 1,
+        label: "How much the field weighs"
     }
 });
 
