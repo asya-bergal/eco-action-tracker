3028919ed81088598f9cd7f446d8ba1588029a8a
Victor <lopezv@mit.edu>
2016-01-21 01:20:40 -0500

    group leaderboard now shows all users

--- client/views/groupViews/editGroup/editGroup.js
+++ client/views/groupViews/editGroup/editGroup.js
@@ -1,4 +1,12 @@
 Template.editGroup.helpers({
+
+  'getActions': function(){
+    if(this.actions){
+      return this.actions.map(function(actionId){
+         return Meteor.users.findOne(actionId);;
+      });
+    }
+  },
   'getUsers': function(){
     if(this.users){
       return this.users.map(function(user){
