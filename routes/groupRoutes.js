4bb22daf9f0954bd228a499676ac0bc02bbbef93
lopezv <lopezv@mit.edu>
2016-01-26 19:02:37 -0500

    cleaned up routes

--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -1,56 +1,56 @@
 // Home Route
 Router.route('/group/:_id', {
-  name: 'group',
-  action: function () {
-    var group = Groups.findOne(this.params._id);      
-    this.render('Group', {        
-      data: function(){
-        return group;
-      }
-    });
-    SEO.set({ title: group.name });
-  }
+    name: 'group',
+    action: function () {
+        var group = Groups.findOne(this.params._id);
+        this.render('Group', {
+            data: function () {
+                return group;
+            }
+        });
+        SEO.set({title: group.name});
+    }
 });
 
 Router.route('/group/:_id/stats', {
-  name: 'groupStats',
-  action: function () {
-    var group = Groups.findOne(this.params._id);
-    this.render('GroupStats', {
-      data: function(){
-        return group;
-      }
-    });
-    SEO.set({ title: group.name });
-  }
+    name: 'groupStats',
+    action: function () {
+        var group = Groups.findOne(this.params._id);
+        this.render('GroupStats', {
+            data: function () {
+                return group;
+            }
+        });
+        SEO.set({title: group.name});
+    }
 });
 
 Router.route('/group', {
-  name: 'groupList',
-  action: function () {
-    this.render('groupList');
-    SEO.set({ title: 'Home - ' + Meteor.App.NAME });
-  }
+    name: 'groupList',
+    action: function () {
+        this.render('groupList');
+        SEO.set({title: 'Home - ' + Meteor.App.NAME});
+    }
 });
 
 Router.route('/group/:_id/newCompetition', {
-  name: 'newCompetition',
-  action: function () {
-    this.render('newCompetition');
-    SEO.set({ title: 'Home - ' + Meteor.App.NAME });
-  }
+    name: 'newCompetition',
+    action: function () {
+        this.render('newCompetition');
+        SEO.set({title: 'Home - ' + Meteor.App.NAME});
+    }
 });
 
 Router.route('/group/:_id/edit', {
-  name: 'editGroup',
-  action: function () {
-    var group = Groups.findOne(this.params._id);      
-    this.render('editGroup', {        
-      data: function(){
-        return group;
-      }
-    });
-    console.log(group);
-    SEO.set({ title: group ? group.name : 'loading' });
-  }
+    name: 'editGroup',
+    action: function () {
+        var group = Groups.findOne(this.params._id);
+        this.render('editGroup', {
+            data: function () {
+                return group;
+            }
+        });
+        console.log(group);
+        SEO.set({title: group ? group.name : 'loading'});
+    }
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,48 +1,36 @@
 //User Routes
-
 Router.route('/user/profile/', {
-  name: 'userProfile',
-  action: function () {
-    if(Meteor.user()){
-      this.render('profile');
-    } else {
-      this.redirect('/')
+    name: 'userProfile',
+    action: function () {
+        if (Meteor.user()) {
+            this.render('profile');
+        } else {
+            this.redirect('/');
+        }
+        SEO.set({title: Meteor.user().username});
     }
-    SEO.set({ title: Meteor.user().username});
-  }
 });
 
-
 Router.route('/user/:_id/history', {
-  name: 'userHistory',
-  action: function () {
-    this.render('history', {
-      data: function(){
-        return Meteor.users.findOne(this.params._id);
-      }
-    });
-    SEO.set({ title: Meteor.user().username });
-  }
+    name: 'userHistory',
+    action: function () {
+        this.render('history', {
+            data: function () {
+                return Meteor.users.findOne(this.params._id);
+            }
+        });
+        SEO.set({title: Meteor.user().username});
+    }
 });
 
 Router.route('/user/:_id/groups', {
-  name: 'userGroups',
-  action: function () {
-    this.render('groups', {
-      data: function(){
-        return Meteor.users.findOne(this.params._id);
-      }
-    });
-    SEO.set({ title: 'Home - ' + Meteor.App.NAME });
-  }
+    name: 'userGroups',
+    action: function () {
+        this.render('groups', {
+            data: function () {
+                return Meteor.users.findOne(this.params._id);
+            }
+        });
+        SEO.set({title: 'Home - ' + Meteor.App.NAME});
+    }
 });
-
-Router.route('/user/takeAction', {
-  name: 'takeAction',
-  action: function () {
-    this.render('takeAction');
-    SEO.set({ title: 'Home - ' + Meteor.App.NAME });
-  }
-});
-
-
