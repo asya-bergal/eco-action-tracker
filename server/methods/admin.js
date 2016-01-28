bf11489dfa885b4eff2cec08dd3796ce4c97f244
lopezv <lopezv@mit.edu>
2016-01-28 15:44:49 -0500

    updated userlist download

--- server/methods/admin.js
+++ server/methods/admin.js
@@ -1,13 +1,21 @@
 Meteor.methods({
-   'addGlobalAdmin': function(userId){
-       check(userId, String);
-       Meteor.users.update(userId, {$set: {'profile.globalAdmin': true}});
-   },
-   'removeGlobalAdmin': function(userId){
-       check(userId, String);
-       Meteor.users.update(userId, {$set: {'profile.globalAdmin': false}});
-   },
-   'isGlobalAdmin': function(){
-       return Meteor.user().profile.globalAdmin;
-   }
+    'addGlobalAdmin': function (userId) {
+        check(userId, String);
+        Meteor.users.update(userId, {$set: {'profile.globalAdmin': true}});
+    },
+    'removeGlobalAdmin': function (userId) {
+        check(userId, String);
+        Meteor.users.update(userId, {$set: {'profile.globalAdmin': false}});
+    },
+    'getUserEmails': function () {
+        var userEmails = Meteor.users.find({}, {fields: {'emails': 1}}).fetch().map(function (u) {
+            return u.emails;
+        }).reduce(function (a, b) {
+            return a.concat(b);
+        }, []).map(function (e) {
+            return e.address;
+        }).join(', ');
+        
+        return userEmails;
+    }
 });
