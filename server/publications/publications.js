78dc9b7c3b81131ad89ae0a6c90a90b6c7c4db8d
Asya Bergal <asyabergal@gmail.com>
2016-02-07 19:07:27 -0500

    Reasonable implementation of only publishing actions relevant to user

--- server/publications/publications.js
+++ server/publications/publications.js
@@ -1,19 +1,52 @@
 Meteor.publish('actions', function() {
-    //TODO: Fix publishing not to publish all actions to the client
-    return Actions.find({});
+    var self = this;
+    var subHandle = Actions.find({}).observeChanges({
+        added: function(id, action) {
+            // TODO: Is there a better way to store the user rather than recompute?
+            var me = Meteor.users.findOne(self.userId, {fields: {"profile.groups": 1}});
+            if(action.isGlobal || contains(me.profile.groups, action.group)) {
+                self.added("actions", id, action);
+            }
+        },
+        changed: function(id, action) {
+            var me = Meteor.users.findOne(self.userId, {fields: {"profile.groups": 1}});
+            if(action.isGlobal || contains(me.profile.groups, action.group)) {
+                self.changed("actions", id, action);
+            }
+        },
+        removed: function(id) {
+            var me = Meteor.users.findOne(self.userId, {fields: {"profile.groups": 1}});
+            if(action.isGlobal || contains(me.profile.groups, action.group)) {
+                self.removed("actions", id);
+            }
+        }
+    });
+
+    self.ready();
+    self.onStop(function () {
+        subHandle.stop();
+    });
 });
 
 Meteor.publish('groups', function() {
-  //TODO: Fix publishing not to publish all groups to the client
-	return Groups.find({});
+    return Groups.find({});
 });
 
 Meteor.publish('competitions', function() {
-    //Publish competitions
     return Competitions.find({});
 });
 
 Meteor.publish('users', function() {
-  //TODO: Fix publishing not to publish all users to the client
-  return Meteor.users.find({});
+    return Meteor.users.find({});
 });
+
+// Return true if arr contains element, else return False
+var contains = function (arr, element) {
+    var i = arr.length;
+    while (i--) {
+        if (arr[i] === element) {
+            return true;
+        }
+    }
+    return false;
+};
