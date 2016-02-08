b03097a7075a6570f8f997115fb20a512359fede
Asya Bergal <asyabergal@gmail.com>
2016-02-07 20:16:53 -0500

    Added taking actions to competition page

--- server/publications/publications.js
+++ server/publications/publications.js
@@ -1,8 +1,10 @@
 Meteor.publish('actions', function() {
+    // This function uses Meteor's low-level publish code to observe changes to Actions.
     var self = this;
+    // For every action, checks whether the action is global or in the user's groups,.
+    // If it is, calls the self.added, self.changed, or self.removed callback as appropriate with that action on the client.
     var subHandle = Actions.find({}).observeChanges({
         added: function(id, action) {
-            // TODO: Is there a better way to store the user rather than recompute?
             var me = Meteor.users.findOne(self.userId, {fields: {"profile.groups": 1}});
             if(action.isGlobal || contains(me.profile.groups, action.group)) {
                 self.added("actions", id, action);
@@ -22,7 +24,9 @@ Meteor.publish('actions', function() {
         }
     });
 
+    // Notify the client that the subscriber knows about all updates.
     self.ready();
+    // Make sure to stop the changes observer when the connection to the client ends.
     self.onStop(function () {
         subHandle.stop();
     });
