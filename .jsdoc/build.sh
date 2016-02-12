7e99cdca37171242e6567ddbfb4aeb6398515dca
Lawrence Wu <llwu@llwu.me>
2016-02-12 09:59:36 -0500

    fixed jsdoc script

--- .jsdoc/build.sh
+++ .jsdoc/build.sh
@@ -1,3 +1,3 @@
 echo "Generating JSDoc Output. Requires 'npm install -g jsdoc ink-docstrap'."
-dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+cd "$(dirname "$0")"
 jsdoc -r .. -R ../README.md -t $(npm config get prefix)/lib/node_modules/ink-docstrap/template -c conf.json
